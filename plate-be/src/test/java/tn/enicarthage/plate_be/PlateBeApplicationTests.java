df5ed08cc7e24daa301cb611f9518c1fbb00983d
hakimBarbaria <hakimbarbaria589@gmail.com>
2026-02-21 11:46:37 +0100

    initialisé le cote back-office du projet avec spring boot

---
+++ plate-be/src/test/java/tn/enicarthage/plate_be/PlateBeApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enicarthage.plate_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
