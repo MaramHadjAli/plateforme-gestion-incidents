38d5db6cf8ed2806bb80c205b2c007775678e09f
hakimBarbaria <hakimbarbaria589@gmail.com>
2026-03-13 13:52:06 +0100

    implementation des relations d'entite Ticket avec les autres entites

--- plate-be/src/main/java/tn/enicarthage/plate_be/entities/Ticket.java
+++ plate-be/src/main/java/tn/enicarthage/plate_be/entities/Ticket.java
@@ -1,6 +1,8 @@
 package tn.enicarthage.plate_be.entities;
 
 import java.util.Date;
+import java.util.List;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +11,8 @@ import lombok.ToString;
 import lombok.EqualsAndHashCode;
 import lombok.Builder;
 
+@Entity
+@Table(name = "tickets")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,12 +21,35 @@ import lombok.Builder;
 @EqualsAndHashCode
 @Builder
 public class Ticket {
+    @Id
     private String idTicket;
+    
     private String titre;
     private String description;
+    
     private Date dateCreation;
+    
     private Date dateCloture;
+    
+    @Enumerated(EnumType.STRING)
     private PRIORITE_TICKET priorite;
+    
+    @Enumerated(EnumType.STRING)
     private STATUS_TICKET status;
+    
     private Date dateLimiteReparation;
+    
+    @ManyToOne
+    @JoinColumn(name = "demandeur_id")
+    private Demandeur createdBy;
+    
+    @ManyToOne
+    @JoinColumn(name = "technicien_id")
+    private Technicien assignedTo;
+    
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Feedback> feedbacks;
+    
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<HistoriqueMaintenance> historiqueMaintenances;
 }
