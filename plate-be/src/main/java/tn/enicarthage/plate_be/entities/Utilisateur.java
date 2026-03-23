da31d41b601df0e08567d31ecdc6433b495f6b9a
MaramHadjAli <maram.hadjali@enicar.ucar.tn>
2026-03-23 18:49:19 +0100

    ajouter entité Utilisateur

--- plate-be/src/main/java/tn/enicarthage/plate_be/entities/Utilisateur.java
+++ plate-be/src/main/java/tn/enicarthage/plate_be/entities/Utilisateur.java
@@ -1,8 +1,29 @@
 package tn.enicarthage.plate_be.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 @Entity
 public class Utilisateur {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private boolean enabled = false;
+
+    @Enumerated(EnumType.STRING)
+    private ROLE role;
 }
