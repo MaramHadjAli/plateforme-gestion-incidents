59dd493984556070a757b19bbb5987293042c6bb
hakimBarbaria <hakimbarbaria589@gmail.com>
2026-03-23 16:45:34 +0100

    création du composant classement

--- plate-fe/src/app/app.routes.ts
+++ plate-fe/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { TicketListComponent } from './plate/technicien/ticket-list/ticket-list.component';
 import { CreateTicketComponent } from './plate/demandeur/create-ticket/create-ticket.component';
+import { ClassementComponent } from './plate/technicien/classement/classement.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/ticket-list', pathMatch: 'full' },
@@ -9,7 +10,7 @@ export const routes: Routes = [
   { path: 'dashboard', component: TicketListComponent },
   { path: 'equipements', component: TicketListComponent },
   { path: 'maintenance', component: TicketListComponent },
-  { path: 'classement', component: TicketListComponent },
+  { path: 'classement', component: ClassementComponent },
   { path: 'profile', component: TicketListComponent },
   { path: 'my-tickets', component: TicketListComponent },
   { path: 'badges', component: TicketListComponent },
