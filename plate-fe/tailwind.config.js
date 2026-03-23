6516a4f67afe7249a1d6b3180cd554e8e06b46ca
hakimBarbaria <hakimbarbaria589@gmail.com>
2026-03-23 14:44:16 +0100

    install flowbite angular package

---
+++ plate-fe/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/**/*.{css,scss}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ]
+};
+
