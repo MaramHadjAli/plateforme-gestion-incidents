11f7e3176be074178ac5268ca9a347b7af14205d
hakimBarbaria <hakimbarbaria589@gmail.com>
2026-03-23 16:43:32 +0100

    corriger les bugs du route dans app-bar component

--- plate-fe/src/app/app.routes.ts
+++ plate-fe/src/app/app.routes.ts
@@ -3,8 +3,16 @@ import { TicketListComponent } from './plate/technicien/ticket-list/ticket-list.
 import { CreateTicketComponent } from './plate/demandeur/create-ticket/create-ticket.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'ticket-list', pathMatch: 'full' },
+  { path: '', redirectTo: '/ticket-list', pathMatch: 'full' },
   { path: 'ticket-list', component: TicketListComponent },
   { path: 'create-ticket', component: CreateTicketComponent },
-  { path: '**', redirectTo: 'ticket-list' },
+  { path: 'dashboard', component: TicketListComponent },
+  { path: 'equipements', component: TicketListComponent },
+  { path: 'maintenance', component: TicketListComponent },
+  { path: 'classement', component: TicketListComponent },
+  { path: 'profile', component: TicketListComponent },
+  { path: 'my-tickets', component: TicketListComponent },
+  { path: 'badges', component: TicketListComponent },
+  { path: 'settings', component: TicketListComponent },
+  { path: '**', redirectTo: '/ticket-list' }
 ];
