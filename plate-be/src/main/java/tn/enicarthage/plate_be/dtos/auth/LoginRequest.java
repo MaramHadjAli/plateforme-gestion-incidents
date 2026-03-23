c2bb240bd2eb6c75a9bdd12a0aecf7802f77e36b
MaramHadjAli <maram.hadjali@enicar.ucar.tn>
2026-03-23 19:01:46 +0100

    creer le package dtos/auth

---
+++ plate-be/src/main/java/tn/enicarthage/plate_be/dtos/auth/LoginRequest.java
@@ -0,0 +1,6 @@
+package tn.enicarthage.plate_be.dtos.auth;
+
+public class LoginRequest {
+    public String email;
+    public String password;
+}
---
+++ plate-be/src/main/java/tn/enicarthage/plate_be/dtos/auth/RegisterRequest.java
@@ -0,0 +1,7 @@
+package tn.enicarthage.plate_be.dtos.auth;
+
+public class RegisterRequest {
+    public String nom;
+    public String email;
+    public String password;
+}
