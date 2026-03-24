a8b4c1f11e970c77e7d5ea2a146d072520a5f030
MaramHadjAli <maram.hadjali@enicar.ucar.tn>
2026-03-24 20:55:01 +0100

    ajouter protection CSRF/XSS/SQLI

--- plate-be/src/main/java/tn/enicarthage/plate_be/dtos/auth/LoginRequest.java
+++ plate-be/src/main/java/tn/enicarthage/plate_be/dtos/auth/LoginRequest.java
@@ -1,7 +1,17 @@
 package tn.enicarthage.plate_be.dtos.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class LoginRequest {
+    
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "L'email doit être valide")
     private String email;
+    
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 6, message = "Le mot de passe doit avoir au moins 6 caractères")
     private final String password;
 
     public LoginRequest(String password) {
